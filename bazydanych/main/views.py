54848136a0e55376be7cbe562708f8a27f8f622c
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-05-21 11:00:57 +0200

    debuging

--- bazydanych/main/views.py
+++ bazydanych/main/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('localhost', 27017)
+print("Client")
+print(client)
+
+db = client['skibidi']
+print("db")
+print(db)
+kolekcja = db['Klasy']['NazwaKlasy']
+print("Kolekcja")
+print(kolekcja)
+wynik = kolekcja.find({})
+print("wynik")
+print(wynik)
+wynik_list = list(wynik)
+print("wynik listy")
+print(wynik_list)
 
 def bazy_danych():
+    print(client.list_database_names())
     return client.list_database_names()
 
 def klucze(baza_danych, kolekcja):
@@ -14,7 +30,8 @@ def klucze(baza_danych, kolekcja):
 
     for document in collection.find():
         kluczyki.append(document.keys())
-
+    print("kluczyki")
+    print(kluczyki)
     return kluczyki
 
 def find(kwerenda, kolekcja, baza_danych):
@@ -41,6 +58,8 @@ def wyswietl_dane(request):
         kolekcja = db['Klasy']['NazwaKlasy']
         wynik = kolekcja.find({})
         wynik_list = list(wynik)
+        print("wynik listy")
+        print(wynik_list)
         return render(request, 'wyswietl.html', {"result": wynik_list})
     else:
         return render(request, 'wyswietl.html')
