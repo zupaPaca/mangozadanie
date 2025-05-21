3709ca69de652cb6eccdddf60c4c97ad6d6e22c5
Gerard <gerard.dziwinski@infotech.edu.pl>
2025-05-21 11:24:33 +0200

    prepare to pull

--- bazydanych/main/views.py
+++ bazydanych/main/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
 from pymongo import MongoClient
+import pprint
 
 client = MongoClient('localhost', 27017)
-print("Client")
-print(client)
 
-db = client['skibidi']
-print("db")
-print(db)
-kolekcja = db['Klasy']['NazwaKlasy']
-print("Kolekcja")
-print(kolekcja)
-wynik = kolekcja.find({})
-print("wynik")
-print(wynik)
-wynik_list = list(wynik)
-print("wynik listy")
-print(wynik_list)
 
 def bazy_danych():
     print(client.list_database_names())
@@ -30,8 +17,7 @@ def klucze(baza_danych, kolekcja):
 
     for document in collection.find():
         kluczyki.append(document.keys())
-    print("kluczyki")
-    print(kluczyki)
+
     return kluczyki
 
 def find(kwerenda, kolekcja, baza_danych):
@@ -54,12 +40,19 @@ def wyswietl_dane(request):
     if request.method == "GET":
         # kolekcja = request.wybrana_kolekcja
         # baza_danych = request.wybrana_baza_danych
-        db = client['skibidi']
-        kolekcja = db['Klasy']['NazwaKlasy']
+        db = client['Klasy']
+        kolekcja = db['NazwaKlasy']
         wynik = kolekcja.find({})
         wynik_list = list(wynik)
-        print("wynik listy")
-        print(wynik_list)
+
         return render(request, 'wyswietl.html', {"result": wynik_list})
     else:
         return render(request, 'wyswietl.html')
+
+def insert(request):
+    if request.method == "POST":
+        db = client["Klasy"]
+        kolekcja = db['NazwaKlasy']
+
+
+    return render(request, 'insert.html')
